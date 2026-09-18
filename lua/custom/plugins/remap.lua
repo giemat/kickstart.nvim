67c65b30639e0d1d844a86a05af84f4a4c16e311
giemat <mgiedroyc@griddynamics.com>
2026-09-18 12:26:45 +0200

    Fix deprecated function in remap

--- lua/custom/plugins/remap.lua
+++ lua/custom/plugins/remap.lua
@@ -46,7 +46,12 @@ vim.diagnostic.config {
   virtual_lines = false, -- Teest shows up underneath the line, with virtual lines
 
   -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
-  jump = { float = true },
+  jump = {
+    on_jump = function(diagnostic, bufnr)
+      if not diagnostic then return end
+      vim.diagnostic.open_float({ bufnr = bufnr, scope = 'cursor', focus = false })
+    end,
+  },
 }
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
